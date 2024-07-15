711cdaf8d5b3a8f192ca2eed84cd046a2440b8ce
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-07-15 15:33:46 -0300

    Feat: Dark theme and theme change

--- materialUI/peoplecitiesapp/src/routes/index.tsx
+++ materialUI/peoplecitiesapp/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts/ThemeContext";
 
 export const AppRoutes = () => {
+    const { toggleTheme } = useAppThemeContext();
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Olá</Button>} />
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}>Olá</Button>} />
             <Route path="*" element={<Navigate to="/pagina-inicial" />}/>
         </Routes>
     )
